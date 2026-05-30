3f4f7a6f034fc53ec9bb09bb75875cb57315bd84
Развалов Никита Игоревич <razvalov2016@yandex.ru>
2026-05-30 21:58:30 +0300

    feat: Добавил скролл наверх при переходе на новый урл

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
 import { AuthProvider } from "./context/AuthContext";
 import { ToastProvider } from "./context/ToastContext";
+import ScrollToTop from "./components/ScrollToTop";
 import LobbyPage from "./pages/LobbyPage";
 import LoginPage from "./pages/LoginPage";
 import OpenRoomsPage from "./pages/OpenRoomsPage";
@@ -12,21 +13,22 @@ import RoomPage from "./pages/RoomPage";
 export default function App() {
   return (
     <BrowserRouter>
-      <ToastProvider>
-        <AuthProvider>
-          <Layout>
-          <Routes>
-            <Route path="/" element={<LobbyPage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/register" element={<RegisterPage />} />
-            <Route path="/rooms" element={<OpenRoomsPage />} />
-            <Route path="/room/:roomId" element={<RoomPage />} />
-            <Route path="/rooms/:roomId/history" element={<RoomHistoryPage />} />
-            <Route path="*" element={<Navigate to="/" replace />} />
-          </Routes>
-          </Layout>
-        </AuthProvider>
-      </ToastProvider>
+      <ScrollToTop />
+        <ToastProvider>
+          <AuthProvider>
+            <Layout>
+            <Routes>
+              <Route path="/" element={<LobbyPage />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/register" element={<RegisterPage />} />
+              <Route path="/rooms" element={<OpenRoomsPage />} />
+              <Route path="/room/:roomId" element={<RoomPage />} />
+              <Route path="/rooms/:roomId/history" element={<RoomHistoryPage />} />
+              <Route path="*" element={<Navigate to="/" replace />} />
+            </Routes>
+            </Layout>
+          </AuthProvider>
+        </ToastProvider>
     </BrowserRouter>
   );
 }
